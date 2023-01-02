a2b8e6840b35724c3a722ffb07f6ce029c558e70
Rustam <antinov.rus@mail.ru>
2023-01-02 21:55:31 +0700

    Make navigation between fragments

--- app/src/main/java/com/example/kfcmain/MainActivity.kt
+++ app/src/main/java/com/example/kfcmain/MainActivity.kt
@@ -2,17 +2,27 @@ package com.example.kfcmain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.kfcmain.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        val navView: BottomNavigationView = binding.bottomBar
+        navView.setupWithNavController(navController)
     }
-
 
 }
